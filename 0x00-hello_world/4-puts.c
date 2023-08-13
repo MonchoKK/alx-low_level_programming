339f116f8e6733e632ce81238bb7a9c439d98f2d
Kabelo <kabelokamogelo96@gmail.com>
2023-08-13 01:19:08 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include<stdio>
+#include <stdio>
 /**
  * main - Entry point
  *
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like 
-building a multiligual puzzle");
-	return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
