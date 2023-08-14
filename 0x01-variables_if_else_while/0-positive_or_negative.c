1a9d51c754fd09ee4e79417bda4d1e79bf8c8aa5
Kabelo <kabelokamogelo96@gmail.com>
2023-08-14 12:44:34 -0700

    Last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,10 +16,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 printf("%d is", n);
 if (n > 0)
-printf("positive\n");
+printf(" positive\n");
 else if(n == 0)
-printf("zero\n");
+printf(" zero\n");
 else
-printf("negative\n");
+printf(" negative\n");
 return (0);
 }
