438059ceb2830a352eb3affef8c8fa9f0b897dd2
Kabelo <kabelokamogelo96@gmail.com>
2023-09-02 11:12:12 -0700

    Write a program that adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 	for (i = 1; i < argc; i++)
 	{
 	e = argv[i];
-	
+
 	for (k = 0; k < strlen(e); k++)
 	{
 	if (e[k] < 48 || e[k] > 57)
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
 	sum += atoi(e);
 	e++;
 	}
-	
+
 	printf("%d\n", sum);
 	}
 	else
