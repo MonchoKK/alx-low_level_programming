1f03d7cde533e4e32a08a5ca417bab88a62cbc85
Kabelo <kabelokamogelo96@gmail.com>
2023-09-14 08:24:19 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,13 +27,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	if (str == NULL)
 		printf("(nil)");
-	else 
+	else
 		printf("%s", str);
-	if (index != (n-1) && separator != NULL)
+	if (index != (n - 1) && separator != NULL)
 		printf("%s", separator);
 	}
 
 	printf("\n");
 	va_end(strings);
 }
-		
