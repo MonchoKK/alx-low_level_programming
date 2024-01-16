da385f45822391a6198bc9a4fe528d3d714b4c30
Kabelo <kabelokamogelo96@gmail.com>
2024-01-16 14:48:18 -0800

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -27,7 +27,7 @@ int word_len(char *str)
 	return (len);
 }
 
-/** count_words _ Counts the number of words contained within a string.
+/** count_words - Counts the number of words contained within a string.
  * @str: The string to be searched.
  *
  * Return: The number of words contained within str.
